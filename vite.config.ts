7e381cf8724a4ba3adfe5cf532d68764ee301810
Jane2727 <janej91@mail.ru>
2023-11-24 02:38:26 +0300

    fix base

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/train-brain/",
+  base: "/",
   plugins: [react()],
   define: {
     // Some libraries use the global object, even though it doesn't exist in the browser.
