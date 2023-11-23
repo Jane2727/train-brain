3d44676c2b92f76242b72399169b4f23e6f8e929
Jane2727 <janej91@mail.ru>
2023-11-24 02:45:53 +0300

    remove base

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/",
   plugins: [react()],
   define: {
     // Some libraries use the global object, even though it doesn't exist in the browser.
